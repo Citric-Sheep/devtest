dd2602134732f76e45f88dbffc06e92edbd41cc2
leo98ber <leonardoberoes94@gmail.com>
2023-05-02 20:04:38 -0400

    feat: add Elevator model

---
+++ elevator_api/elevator_api/elevator/models/elevators.py
@@ -0,0 +1,17 @@
+"""Call model."""
+
+# Django
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+
+class Elevator(models.Model):
+    """A Django model for representing an elevator."""
+
+    name = models.CharField(max_length=50)
+    current_floor = models.IntegerField(default=0)
+    max_floors = models.IntegerField(validators=[MinValueValidator(2), MaxValueValidator(163)])
+
+    def __str__(self):
+        """Returns a string representation of the elevator object."""
+        return self.name
