5a6a593f51d13c7a83b2db99f11c2ae2bf6c6301
leo98ber <leonardoberoes94@gmail.com>
2023-05-02 20:02:20 -0400

    feat: add app elevator

---
+++ elevator_api/elevator_api/elevator/apps.py
@@ -0,0 +1,11 @@
+
+"""Elevator app."""
+
+from django.apps import AppConfig
+
+
+class ElevatorAppConfig(AppConfig):
+    """Elecator app config."""
+
+    name = 'elevator_api.elevator'
+    verbose_name = 'Elevators'
