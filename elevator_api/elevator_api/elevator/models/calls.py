84ed5065f70e14f96e334590d21234b8e33491c1
leo98ber <leonardoberoes94@gmail.com>
2023-05-02 20:04:46 -0400

    feat: add Call model

---
+++ elevator_api/elevator_api/elevator/models/calls.py
@@ -0,0 +1,19 @@
+"""Call model."""
+
+# Django
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Validation parameters
+validators = {'validators': [MinValueValidator(0), MaxValueValidator(163)],
+              'blank': False}
+
+
+class Call(models.Model):
+    """A Django model for representing an elevator call."""
+
+    moment = models.DateTimeField()
+    source_floor = models.IntegerField(**validators)
+    destination_floor = models.IntegerField(**validators)
+    elevator = models.ForeignKey('elevator.Elevator', on_delete=models.CASCADE, null=True)
+    rest_floor = models.IntegerField(**validators)
