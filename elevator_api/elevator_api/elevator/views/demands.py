6807159d6468dd2620ef2915378b2cc50ea7f954
leo98ber <leonardoberoes94@gmail.com>
2023-05-03 23:10:11 -0400

    feat: add list_ml_demand_data method (endpoint)

--- elevator_api/elevator_api/elevator/views/demands.py
+++ elevator_api/elevator_api/elevator/views/demands.py
@@ -1,5 +1,12 @@
 # Django Rest Framework Imports
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
+# Machine Learning Imports
+import numpy as np
 
 # View imports
 from elevator_api.elevator.serializers.demands import DemandGetSerializer, DemandSerializerCreate
@@ -11,10 +18,29 @@ class DemandViewSet(ModelViewSet):
 
     serializer_class = DemandGetSerializer
     model_class = serializer_class.Meta.model
-    queryset = model_class.objects.all()
+
+    def get_queryset(self):
+        queryset = self.model_class.objects.all()
+        elevator = self.request.query_params.get('elevator', None)
+        if elevator is not None:
+            queryset = queryset.filter(elevator=elevator)
+        return queryset
 
     def get_serializer_class(self):
         """Return serializer based on action."""
         if self.action == 'create' or self.action == 'update':
             return DemandSerializerCreate
         return self.serializer_class
+
+    @action(detail=False, methods=['get'])
+    def list_ml_demand_data(self, request):
+        queryset = self.get_queryset()
+        data = self.get_serializer(queryset, many=True).data
+
+        process_data = np.array([[d['elevator'], d['moment'],
+                                  d['source_floor'], d['destination_floor'],
+                                  d['rest_floor'], d['max_floors']]
+                                 for d in data])
+
+        response_data = {'data': process_data.tolist()}
+        return Response(response_data, status=status.HTTP_200_OK)
