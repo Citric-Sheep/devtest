1d846f6f46f3ead7d039de17a852abf7d5c09770
Leonardo Beroes <leonardoberoes94@gmail.com>
2023-05-04 11:50:37 -0400

    feat: add validation error for max_floor in demand

--- elevator_api/elevator_api/elevator/serializers/demands.py
+++ elevator_api/elevator_api/elevator/serializers/demands.py
@@ -26,3 +26,18 @@ class DemandSerializerCreate(serializers.ModelSerializer):
     class Meta:
         model = Demand
         fields = '__all__'
+
+    def validate(self, validated_data):
+        elevator_max_floors = validated_data['elevator'].max_floors
+        source_floor = validated_data['source_floor']
+        destination_floor = validated_data['destination_floor']
+        rest_floor = validated_data['rest_floor']
+
+        if elevator_max_floors < source_floor or \
+            elevator_max_floors < destination_floor or \
+                elevator_max_floors < rest_floor:
+
+            raise serializers.ValidationError('Floor number must be a less or equal '
+                                              'to max_floors elevator')
+
+        return validated_data
