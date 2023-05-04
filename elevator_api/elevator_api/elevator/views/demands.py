44bc322d504746bb09bc35fa2c2e85cb979f375a
Leonardo Beroes <leonardoberoes94@gmail.com>
2023-05-04 11:00:56 -0400

    refactor: remove unnecessary import

--- elevator_api/elevator_api/elevator/views/demands.py
+++ elevator_api/elevator_api/elevator/views/demands.py
@@ -3,7 +3,6 @@ from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from django_filters.rest_framework import DjangoFilterBackend
 
 # Machine Learning Imports
 import numpy as np
