74baf7591ad61209ee7976acf942fe8a78b8e1b3
SamuelMalaga <samuelmalaga99@gmail.com>
2024-02-02 07:09:35 -0300

    Build: configured the project basic build and models

---
+++ elevator_api/elevator/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from elevator import views
+#from .views import PersonViewSet,ElevatorCallViewSet,MovementViewSet,ElevatorViewSet
+
+router = DefaultRouter()
+# router.register(r'elevator_calls', ElevatorCallViewSet, basename='elevator_calls')
+# router.register(r'elevator', ElevatorViewSet, basename='elevator')
+# router.register(r'movement', MovementViewSet, basename='movement')
+# router.register(r'person', PersonViewSet, basename='person')
+
+#router.register(r'people', PersonViewSet, basename='person')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
