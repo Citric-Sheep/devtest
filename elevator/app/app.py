456fb133465386b9a160d60eef168078edc41c11
Felipeuribe81 <felipenorg@hotmail.es>
2024-04-01 18:26:21 -0500

    init of the python modules and app.py updated without init routes

--- elevator/app/app.py
+++ elevator/app/app.py
@@ -2,8 +2,6 @@ from flask import Flask
 from db import elevator
 from routes.elevator_routes import elevator_routes, elevator_api_routes
 
-from routes import init_routes
-
 
 def create_app():
     flask_app = Flask(__name__)
@@ -11,7 +9,6 @@ def create_app():
     elevator.create_database()
     elevator.create_elevator_tables()
 
-    flask_app.register_blueprint(init_routes)
     flask_app.register_blueprint(elevator_routes)
     flask_app.register_blueprint(elevator_api_routes)
 
