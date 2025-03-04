313d1f6f18a655a06a52fcac54a5482646a396d0
Raul Quinzani <raultq1@hotmail.com>
2025-03-04 00:16:16 -0300

    added absolute path to the DB

--- elevators_app/config.py
+++ elevators_app/config.py
@@ -2,5 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or ''
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///instance/elevator.db'
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f'sqlite:///{os.path.join(basedir, "instance", "elevator.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
