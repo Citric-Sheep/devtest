e25b16d99ded49feef84519438a2a5290ccf7a0a
hexwave <hexwave7@gmail.com>
2025-07-01 02:31:05 -0400

    Revert "FastAPI,PostgreSQL,Alembic"
    This reverts commit eb4bc701cee8a5a46d8f4b3e98a6fb85bfa43a1d.

--- scripts/docker_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script to run the tests for the elevator demand prediction system in Docker.
-
-# Build the containers if they don't exist
-docker-compose build
-
-# Run the tests
-docker-compose run --rm api python -m pytest -v src/tests
--- src/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# This file makes the models directory a Python package
-from src.models.elevator import Elevator
-from src.models.elevator_demand import ElevatorDemand
